874a4197b197c89ee096f3e430c48d486693d337
andersoncristiano94 <andersonzbsi@gmail.com>
2024-07-29 10:33:15 -0300

    Aula Objetos - Softex

---
+++ objetos.js
@@ -0,0 +1,48 @@
+/*const pessoa ={
+  nome : "João",
+  idade:30,
+  saudacao: function(){
+    return `olá, meu nome é ${this.nome} e eu tenho ${this.idade} anos`;
+  }
+}
+console.log(pessoa.saudacao());
+
+const carro ={
+  marca: "Toyota",
+  modelo:"Corolla",
+  ano: 2020,
+  informe: function(){
+
+    return `Seu carro é da marca ${this.marca}, modelo ${this.modelo} e teve seu lançamento no ano de ${this.ano}.`
+  }
+}
+
+console.log(carro.informe(2015));
+
+const livro ={
+  titulo: "Livro da Noite",
+  autor: "Holly Biack",
+  anoPublicacao: 2022
+}
+console.log(livro);
+*/
+
+var biblioteca =[ 
+    {
+    titulo: "A história secreta",
+    autor: "Donna Tartt",
+    anoPublicacao: 2022
+  } ,
+  {
+    titulo: "O amor nos tempos do cólera",
+    autor: "Gabriel García Márquez Tartt",
+    anoPublicacao: 2000
+  } ,
+  {
+    titulo: "O olho mais azul",
+    autor: " Toni Morrison",
+    anoPublicacao: 2000
+  } ,
+]
+
+console.log(biblioteca[1])
